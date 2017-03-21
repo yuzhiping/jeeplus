0149f4642c030304b9dc0869d89c595d169c6530
yuzhiping <yuzhiping140103@hotmail.com>
2017-03-21 22:33:13 +0800

    1.新增修改永久素材API 2.更新fastjson版本

--- jeeplus-weixin/src/main/java/com/jeeplus/weixin/fastweixin/api/MaterialAPI.java
+++ jeeplus-weixin/src/main/java/com/jeeplus/weixin/fastweixin/api/MaterialAPI.java
@@ -214,6 +214,23 @@ public class MaterialAPI extends BaseAPI {
         return ResultType.get(response.getErrcode());
     }
 
+    /**
+     * 修改永久图文素材
+     * @param mediaId 要修改的图文消息的id
+     * @param index 要更新的文章在图文消息中的位置（多图文消息时，此字段才有意义），第一篇为0
+     * @param articles
+     * @return
+     */
+    public ResultType updateMaterial(String mediaId,int index,List<Article> articles){
+        String url=BASE_API_URL+"cgi-bin/material/update_news?access_token=#";
+        final Map<String,Object> param=new HashMap<String,Object>();
+        param.put("media_id",mediaId);
+        param.put("index",index);
+        param.put("articles",articles);
+        BaseResponse response=executePost(url,JSONUtils.toJson(param));
+        return ResultType.get(response.getErrcode());
+    }
+
     private void downloadVideo(DownloadMaterialResponse response){
         String url = response.getDownUrl();
         LOG.debug("Download url: " + url);
